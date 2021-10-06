c27971a674ceea2c7f6437c20d0dc838edff7d8e
Ra-tnt <roody.tnt@gmail.com>
2021-10-06 11:13:32 +0000

    added another function called check

--- file1.java
+++ file1.java
@@ -4,7 +4,7 @@ public static void main (String [] args){
 int x = 0;
 int y = 10;
 String name = "Raghad";
-
+System.out.Println(check(name));
 
 
 }
@@ -13,4 +13,13 @@ public static void myFunc(String name){
 
 	System.out.println(name);
 }
+}
+
+public static boolean check(String name){
+
+	boolean x = true;
+	if (name.equals("Roody")
+		x = false;
+
+	return x;
 }
