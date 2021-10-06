aaf6d98962e94079a32943da20ea87b1a01fdfaa
Ra-tnt <roody.tnt@gmail.com>
2021-10-06 10:44:31 +0000

    my file

---
+++ file1.java
@@ -0,0 +1,16 @@
+public class hello{
+public static void main (String [] args){
+
+int x = 0;
+int y = 10;
+String name = "Raghad";
+
+
+
+}
+
+public static void myFunc(String name){
+
+	System.out.println(name);
+}
+}
