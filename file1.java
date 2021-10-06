d1c8c373e20045fc91b7c5971411f72f8c172ab0
Ra-tnt <roody.tnt@gmail.com>
2021-10-06 11:34:32 +0000

    comment

--- file1.java
+++ file1.java
@@ -4,6 +4,7 @@ public static void main (String [] args){
 int x = 0;
 int y = 10;
 String name = "Raghad";
+//this should be for testing the real call should be done inside the first function "myFunc"
 System.out.Println(check(name));
 
 
