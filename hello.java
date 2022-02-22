3dcd208c2026b98b548b0b482a74536751265413
Raghad <ralqahtani@lucidya.com>
2022-02-22 13:35:38 +0300

    exp

--- hello.java
+++ hello.java
@@ -1,7 +1,14 @@
+
+
 public class hello{
 public static void main (String [] args){
 
 System.out.println("Hello world");
+System.out.println(doingExp(4));
 
+}
+public static String doingExp(int num){
+
+   return ("result is "+ num*num)
 }
 }
