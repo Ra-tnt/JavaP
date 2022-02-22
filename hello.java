e33650a558ee6ec014703e9f15f380d510b1e178
Raghad <ralqahtani@lucidya.com>
2022-02-22 14:50:56 +0300

    adding this for testing

--- hello.java
+++ hello.java
@@ -2,6 +2,11 @@ public class hello{
 public static void main (String [] args){
 
 System.out.println("Hello world");
+TestingBranch();
 
+}
+public static void TestingBranch(){
+
+    System.out.print("Hi everty one from testing")
 }
 }
