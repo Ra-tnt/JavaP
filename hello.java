5a9759b21aac35bcb668f4513292aa8ee5e82eb3
Raghad <ralqahtani@lucidya.com>
2022-02-22 15:23:51 +0300

    Revert "rebased"
    This reverts commit 612e13fb1c542b7dc376ebc97f5e0a0dd131bd3a.

--- hello.java
+++ hello.java
@@ -8,12 +8,7 @@ System.out.println(doingExp(4));
 
 }
 public static String doingExp(int num){
-TestingBranch();
 
    return ("result is "+ num*num)
-}
-public static void TestingBranch(){
-
-    System.out.print("Hi everty one from testing")
 }
 }
