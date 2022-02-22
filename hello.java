1cd4d23047acf26d1dcb01113d29cc6cfd1251b5
Raghad <ralqahtani@lucidya.com>
2022-02-22 15:34:11 +0300

    doing

--- hello.java
+++ hello.java
@@ -4,14 +4,11 @@ public class hello{
 public static void main (String [] args){
 
 System.out.println("Hello world");
-<<<<<<< HEAD
 System.out.println(doingExp(4));
 
 }
 public static String doingExp(int num){
-=======
 TestingBranch();
->>>>>>> e33650a (adding this for testing)
 
    return ("result is "+ num*num)
 }
